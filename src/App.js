5ee1863e3d7306af410b0bc33cffb9991da8e604
rajibdas-it <rajibrad@gmail.com>
2022-11-23 21:51:49 +0600

    added favicon and change title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-[1440px] border border-red-500 mx-auto">
       <h1>Assignment 12</h1>
     </div>
   );
