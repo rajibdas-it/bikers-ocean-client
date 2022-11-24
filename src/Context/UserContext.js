b22b4e868bb8aabfad5c8e294036bc6577a9e607
rajibdas-it <rajibrad@gmail.com>
2022-11-24 15:25:23 +0600

    added firebase authentication  methods

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,12 +1,65 @@
-import React, { createContext } from "react";
-import { getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import app from "../Firebase/firebase.config";
 
 export const AuthProvider = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
-  const authInfo = { name: "Rajib Das" };
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const userRegistration = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserInfo = (userInfo) => {
+    return updateProfile(auth.currentUser, userInfo);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    userRegistration,
+    googleSignIn,
+    userLogin,
+    updateUserInfo,
+    logOut,
+  };
   return (
     <div>
       <AuthProvider.Provider value={authInfo}>{children}</AuthProvider.Provider>
