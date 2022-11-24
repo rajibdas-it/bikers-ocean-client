5b59d62f9adc4f72794a3adf142cb970d8d1570c
rajibdas-it <rajibrad@gmail.com>
2022-11-24 11:19:03 +0600

    added extra section and footer

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -3,7 +3,7 @@ import Category from "../Category/Category";
 
 const Categories = () => {
   return (
-    <div>
+    <div className="my-10">
       <h1 className="text-center text-4xl font-bold mb-10">Category</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <Category></Category>
