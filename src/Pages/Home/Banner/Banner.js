e41a7dc9538b22344a34ce198a2d80306f184e74
rajibdas-it <rajibrad@gmail.com>
2022-11-26 17:25:13 +0600

    add section complete

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -16,56 +16,65 @@ import four from "../../../assets/slider-img/cbr.png";
 import five from "../../../assets/slider-img/cbr.png";
 import six from "../../../assets/slider-img/cbr.png";
 import seven from "../../../assets/slider-img/cbr.png";
+import { useQuery } from "@tanstack/react-query";
 
 const Banner = () => {
+  const { data: addvertiseItem = [], isLoading } = useQuery({
+    queryKey: ["AdvertiseProduct"],
+    queryFn: () =>
+      fetch("http://localhost:5000/createAdvertise").then((res) => res.json()),
+  });
+
+  // console.log(addvertiseItem);
+
   return (
-    <div className=" my-16 border border-red-500">
-      <Swiper
-        effect={"coverflow"}
-        slidesPerView={2}
-        spaceBetween={10}
-        centeredSlides={true}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 50,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img className="h-[40vh] rounded" src={one} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40vh] rounded" src={two} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40vh] rounded" src={three} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40vh] rounded" src={four} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40vh] rounded" src={five} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40vh] rounded" src={six} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40vh] rounded" src={seven} alt="" />
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    <>
+      {addvertiseItem.length > 0 && (
+        <div className=" my-16 border border-red-500">
+          <h2 className="text-center font-bold text-4xl my-5">
+            New Arrived
+            <span className="badge badge-lg text-primary">Hot Collections</span>
+          </h2>
+
+          <Swiper
+            effect={"coverflow"}
+            slidesPerView={2}
+            spaceBetween={10}
+            centeredSlides={true}
+            autoplay={{
+              delay: 1500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 50,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+            className="mySwiper"
+          >
+            {addvertiseItem.map(
+              (item) =>
+                item.status === "available" && (
+                  <SwiperSlide key={item?._id}>
+                    <img
+                      className="h-[40vh] rounded"
+                      src={item?.image}
+                      alt=""
+                    />
+                  </SwiperSlide>
+                )
+            )}
+          </Swiper>
+        </div>
+      )}
+    </>
   );
 };
 
