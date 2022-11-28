e796326bf6f4a69f99aada08deccb48e65605f40
rajibdas-it <rajibrad@gmail.com>
2022-11-28 17:09:32 +0600

    error page done

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import errorImg from "../../assets/bike.gif";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+
   return (
-    <div>
-      <h1>404 Page Not Found</h1>
+    <div className=" w-[70%] mx-auto">
+      <div className="w-[60%] mx-auto mt-5">
+        <img src={errorImg} alt="" />
+      </div>
+      <div>
+        {error && (
+          <div>
+            <h1 className="text-center text-5xl font-bold italic mb-5">
+              {error.status}
+            </h1>
+            <p className="text-center text-lg font-bold italic">
+              {error.statusText || error.message}
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className="text-center mt-5">
+        <Link to="/">
+          <button className="btn btn-primary capitalize">
+            Back To Home Page
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
